c4696dc07f0765c017b90446a1360a141e5d56ab
Steve-design <smzalendo31@gmail.com>
2019-10-28 17:13:57 +0300

    update tests

--- tests/test_case.py
+++ tests/test_case.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User,Post, Comment
+from app.models import User,Post, Comment, Role
 from app import db
 
 class UserTest(unittest.TestCase):
@@ -36,22 +36,3 @@ class PostTest(unittest.TestCase):
 class CommentTest(unittest.TestCase):
     def setUp(self):
         self.new_comment = Comment(1,"comment",1,1)
-
-    # def test_instance(self):
-    #     self.assertTrue(isinstance(self.new_comment,Comment))
-
-    # def test_init(self):
-    #     self.assertEqual(self.new_comment.id, 1)
-    #     self.assertEqual(self.new_comment.comment, "comment")
-    #     self.assertEqual(self.new_comment.user_id, 1)
-    #     self.assertEqual(self.new_comment.post_id, 1)
-
-    # def test_save_comment(self):
-    #     self.new_comment.save_comment()
-    #     self.assertTrue(len(Comment.query.all()) > 0)
-
-
-    # def test_get_comment_by_post_id(self):
-    #     self.new_comment.save_comment()
-    #     get_comments = Comment.get_comments(12345)
-    #     self.assertTrue(len(get_comments) == 1)
