7d70ad64816c6f8498bc7f3540e6e7ab7d3a0164
Steve-design <smzalendo31@gmail.com>
2019-10-28 17:23:53 +0300

    update models

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
+    username = db.Column(db.String(255),unique = True)
     email = db.Column(db.String(255),unique = True,index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
